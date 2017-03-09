01228ed8b0392b15ca2780c58415d57aaa6551eb
sepehrfakour <sepehrfakour@users.noreply.github.com>
2017-03-09 01:32:18 -0800

    Add Gulp tasks for replacing asset paths on different environments

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var less = require('gulp-less-sourcemap');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const less = require('gulp-less-sourcemap');
+const replace = require('gulp-replace');
+
+const webpackDevServerUrl = 'http://localhost:8080';
 
 // Static Server
 gulp.task('serve', function() {
@@ -40,7 +43,35 @@ gulp.task('less', function() {
     .pipe(browserSync.stream());
 });
 
+// Prepare app.js bundle path for production
+gulp.task('production-js', function(){
+    var filenames = [
+        './index.html'
+    ];
+    filenames.forEach( function (name) {
+        gulp.src([name], {base: './'})
+            .pipe(replace( webpackDevServerUrl + '/app.js', '/assets/js/app.js'))
+            .pipe(gulp.dest('./'));
+    });
+});
+// Prepare app.js bundle path for development
+gulp.task('dev-js', function(){
+    var filenames = [
+        './index.html'
+    ];
+    filenames.forEach( function (name) {
+        gulp.src([name], {base: './'})
+            .pipe(replace('/assets/js/app.js', webpackDevServerUrl + '/app.js'))
+            .pipe(gulp.dest('./'));
+    });
+});
+
 
 gulp.task('default', ['serve']);
 gulp.task('server', ['serve']);
-gulp.task('dev', ['watch']);
+gulp.task('dev', ['dev-js','watch'], function () {
+    console.log('Asset paths ready for development');
+});
+gulp.task('production', ['production-js'], function () {
+    console.log('Asset paths ready for production');
+});
