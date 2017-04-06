ab261d66f240b72925f57f4610b96de298d2d788
Sepehr <sepehrfakour@users.noreply.github.com>
2017-04-05 17:48:57 -0700

    Update to use new Algolia account

--- client/app.jsx
+++ client/app.jsx
@@ -7,8 +7,8 @@ const ReactDOM = require('react-dom');
 const Main = require('./components/MainComponent.jsx').default;
 
 // Algolia App ID and Public Search Key global variables
-window.ALGOLIA_APPLICATION_ID = 'ABF3QSSV0C';
-window.ALGOLIA_SEARCH_API_KEY = 'e3d710ef8f7b6bc229911927236200ae';
+window.ALGOLIA_APPLICATION_ID = '9KFLYOY1GK';
+window.ALGOLIA_SEARCH_API_KEY = 'd219521c2e9a0cf7395b485789a88d4c';
 window.ALGOLIA_INDEX_NAME = 'restaurants';
 
 ReactDOM.render(
